c390c5665970810b20159616221f97396e8b1c28
jpoupinet <j.poupinet@gmail.com>
2021-04-30 12:02:20 +0200

    ajout header et favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 import Navbar from '../components/Navbar';
 import '../all.sass';
+import useSiteMetadata from './SiteMetadata';
+import { withPrefix } from 'gatsby';
 
 const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
+
   return (
     <div>
+      <Helmet>
+        <html lang="fr" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          sizes="16x16"
+        />
+
+        <link
+          rel="mask-icon"
+          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          color="#4d6b96"
+        />
+        <meta name="theme-color" content="#4d6b96" />
+
+        <meta property="og:type" content="business.business" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="/" />
+        <meta
+          property="og:image"
+          content={`${withPrefix('/')}img/og-image.jpg`}
+        />
+      </Helmet>
       <Navbar />
       <div>{children}</div>
     </div>
