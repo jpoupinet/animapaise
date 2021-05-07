6fd5d9ff368d8a59097db6f6a2e0e5343e01f91c
jpoupinet <j.poupinet@gmail.com>
2021-05-07 21:13:07 +0200

    répare betise git

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,48 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/img`,
+        name: `uploads`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
         path: `${__dirname}/src/pages`,
       }
     },
-    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              staticFolderName: 'static',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          }
+        ],
+      },
+    },
     `gatsby-plugin-netlify-cms`
   ]
 }
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,15 +3,72 @@ import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
+import Features from '../components/Features';
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
+import Presentation from '../components/Presentation';
 
-export const IndexPageTemplate = ({
-  title,
+const IndexPageTemplate = ({
+  mainpitch,
+  intro,
+  presentations,
 }) => (
-  <div>{title}</div>
+  <div>
+    <section className="hero is-fullheight-with-navbar">
+      <div className="hero-body">
+        <div className="container is-fluid">
+          <div className="columns is-vcentered">
+            <div className="column"></div>
+            <div className="column is-two-fifths">
+              <div className="container">
+                <h2 className="title is-2">{mainpitch.title}</h2>
+                <div className="block">{mainpitch.description}</div>
+              </div>
+            </div>
+            <div className="column"></div>
+            <div className="column is-two-fifths">
+              <div className="container has-text-centered">
+                <PreviewCompatibleImage
+                  imageInfo={{ alt: 'Animapaise logo', image: mainpitch.image }}
+                />
+              </div>
+            </div>
+            <div className="column"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="section has-background-primary">
+      <div className="container">
+        <div className="block has-text-white">
+          <h1 className="title is-1 has-text-white">{intro.heading}</h1>
+          <p>{intro.description}</p>
+        </div>
+        <Features gridItems={intro.blurbs} />
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
+        {presentations.map((pres, i) =>
+          <Presentation
+            photo={pres.photo}
+            titre={pres.titre}
+            description={pres.description}
+            positionPhoto={i % 2 === 0 ? 'gauche' : 'droite'}
+          />
+        )}
+      </div>
+    </section>
+  </div>
 );
 
 IndexPageTemplate.propTypes = {
-  title: PropTypes.string,
+  mainpitch: PropTypes.object,
+  intro: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    blurbs: PropTypes.array,
+  }),
+  presentations: PropTypes.array,
 };
 
 const IndexPage = ({ data }) => {
@@ -20,7 +77,9 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        title={frontmatter.title}
+        mainpitch={frontmatter.mainpitch}
+        intro={frontmatter.intro}
+        presentations={frontmatter.presentations}
       />
     </Layout>
   )
@@ -40,7 +99,46 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
-        title
+        mainpitch {
+          title
+          description
+          image {
+            childImageSharp {
+              gatsbyImageData(
+                width: 800
+                quality: 64
+              )
+            }
+          }
+        }
+        intro {
+          heading
+          description
+          blurbs {
+            image {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 180
+                  quality: 64
+                )
+              }
+            }
+            text
+          }
+        }
+        presentations {
+          photo {
+            childImageSharp {
+              gatsbyImageData(
+                width: 1000
+                height: 800
+                quality: 64
+              )
+            }
+          }
+          titre
+          description
+        }
       }
     }
   }
