2dc69e17cb41e85fc2df4434368d866df700e887
jpoupinet <j.poupinet@gmail.com>
2021-05-09 22:22:44 +0200

    correction path preview index

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,5 @@
 import CMS from 'netlify-cms-app';
 
-import IndexPagePreview from './preview-templates/IndexPagePreview';
+import IndexPagePreview from './IndexPagePreview';
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
