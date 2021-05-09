27bf0f81222d4676f80b04b40188bb2d313d5ad1
jpoupinet <j.poupinet@gmail.com>
2021-05-09 22:17:33 +0200

    ajout preview index pour cms

---
+++ src/cms/IndexPagePreview.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IndexPageTemplate } from '../templates/index-page';
+
+const IndexPagePreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(['data']).toJS();
+
+  if (data) {
+    return (
+      <IndexPageTemplate
+        mainpitch={data.mainpitch || {}}
+        intro={data.intro || {}}
+        presentations={data.presentations || {}}
+        decouvrir={data.decouvrir || {}}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+}
+
+IndexPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  getAsset: PropTypes.func,
+};
+
+export default IndexPagePreview;
