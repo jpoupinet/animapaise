771cf97abfd92ac831e6a26511c9906212801fdb
jpoupinet <j.poupinet@gmail.com>
2021-05-10 19:46:20 +0200

    séparation template et template wrapper index

---
+++ src/templates/IndexPageTemplate.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+
+import Features from '../components/Features';
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage';
+import PreviewCompatibleBgImage from '../components/PreviewCompatibleBgImage';
+import Presentation from '../components/Presentation';
+
+const IndexPageTemplate = ({
+  mainpitch,
+  intro,
+  presentations,
+  decouvrir,
+}) => {
+  const [width, setWidth] = useState(null);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const handleResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    };
+  });
+
+  return (
+    <div>
+      <section className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container is-fluid is-flex">
+            <div className="columns is-vcentered">
+              <div className="column"></div>
+              <div className="column is-two-fifths">
+                <div className="container">
+                  <h2 className="title is-2">{mainpitch.title}</h2>
+                  <div className="block">{mainpitch.description}</div>
+                </div>
+              </div>
+              <div className="column"></div>
+              <div className="column is-two-fifths">
+                <div className="container has-text-centered">
+                  <PreviewCompatibleImage
+                    imageInfo={{ alt: 'Animapaise logo', image: mainpitch.image }}
+                  />
+                </div>
+              </div>
+              <div className="column"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section has-background-primary">
+        <div className="container">
+          <div className="block has-text-white">
+            <h1 className="title is-1 has-text-white">{intro.heading}</h1>
+            <p>{intro.description}</p>
+          </div>
+          <Features gridItems={intro.blurbs} />
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          {presentations.map((pres, i) =>
+            <Presentation
+              key={`presentation${i}`}
+              photo={pres.photo}
+              titre={pres.titre}
+              description={pres.description}
+              positionPhoto={(i % 2 === 0 || width <= 768) ? 'gauche' : 'droite'}
+            />
+          )}
+        </div>
+      </section>
+      <PreviewCompatibleBgImage
+        bgImageClass="hero is-halfheight"
+        imageInfo={{ image: decouvrir.image }}
+      >
+        <div className="
+          hero-body
+          is-flex-direction-column
+          is-justify-content-space-around
+          is-align-items-flex-start
+        ">
+          <h2
+            className="title is-2 has-text-white"
+            style={{ width: `${width <= 768 ? 100 : 40}%`, textShadow: '1px 1px 2px black' }}
+          >
+            {decouvrir.title}
+          </h2>
+          <div className="block">
+            <Link to="/" className="button is-primary is-large">{decouvrir.lien}</Link>
+          </div>
+        </div>
+      </PreviewCompatibleBgImage>
+    </div>
+  )
+};
+
+IndexPageTemplate.propTypes = {
+  mainpitch: PropTypes.object,
+  intro: PropTypes.shape({
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    blurbs: PropTypes.array,
+  }),
+  presentations: PropTypes.array,
+  decouvrir: PropTypes.object,
+};
+
+export default IndexPageTemplate;
