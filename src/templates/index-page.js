4c009b2e8d7a6007c67184757c404b6ec5121def
jpoupinet <j.poupinet@gmail.com>
2021-05-12 00:38:19 +0200

    test fix

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -15,7 +15,7 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
         presentations={frontmatter.presentations}
         decouvrir={frontmatter.decouvrir}
-        wrapperWindow={typeof window !== `undefined` ? window : null}
+        wrapperWindow={window ? window : null}
       />
     </Layout>
   )
