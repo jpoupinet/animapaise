cbbb5186d5acea6cc1ad35518cb22d7df19b42db
jpoupinet <j.poupinet@gmail.com>
2021-05-17 10:49:12 +0200

    fix oublis de nettoyage page construction

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 
 import logo from '../../static/img/logo.svg';
 import logoTexte from '../../static/img/logo-texte.svg';
 
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
-
+const IndexPage = () => {
   return (
     <Layout>
       <section className="hero is-fullheight is-primary has-text-centered">
@@ -35,14 +31,6 @@ const IndexPage = ({ data }) => {
       </section>
     </Layout>
   )
-};
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
 };
 
 export default IndexPage;
