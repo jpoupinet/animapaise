df159ede4e265e60455e20fd422aba29b93de313
Julien Poupinet <jpoupinet@gmail.com>
2022-04-04 13:20:36 +0200

    ajout plugin gatsby sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: 'gatsby-transformer-remark',
       options: {
